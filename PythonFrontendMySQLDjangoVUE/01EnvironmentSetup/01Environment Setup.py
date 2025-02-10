00962d5602d89e86e8e93f15ee55fb3b690895cb
COCO-WaterBird <zhangkk533@gmail.com>
2025-02-09 22:20:43 -0800

    new_learn

---
+++ PythonFrontendMySQLDjangoVUE/01EnvironmentSetup/01Environment Setup.py
@@ -0,0 +1,9 @@
+print(int("8") > 7)          # 把字符串 "8" 转换为整数后判断是否大于 7
+print(str(111) == 111)       # 比较字符串 "111" 和整数 111 是否相等
+print(bool(-1))              # 将 -1 转换为布尔值
+print(bool(0))               # 将 0 转换为布尔值
+print(bool(""))              # 将空字符串转换为布尔值
+print(bool("你好"))          # 将非空字符串 "你好" 转换为布尔值
+print(True == True)          # 比较 True 和 True 是否相等
+print(True == False)         # 比较 True 和 False 是否相等
+print(bool("") == bool(0))   # 比较空字符串转换的布尔值和 0 转换的布尔值是否相等
